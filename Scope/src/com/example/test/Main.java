16ddc3c145c9de02dec576a4440465bbe481f387
michaelyatco <mjyatco@gmail.com>
2018-02-17 11:35:43 -0500

    Scope vs Visibility

--- Scope/src/com/example/test/Main.java
+++ Scope/src/com/example/test/Main.java
@@ -3,15 +3,19 @@ package com.example.test;
 public class Main {
 
     public static void main(String[] args) {
-	    String privateVar = "this is private to main()";
+	    String varFour = "this is private to main()";
 
 	    ScopeCheck scopeInstance = new ScopeCheck();
-	    System.out.println("scopeInstance privateVar is " + scopeInstance.getPrivateVar());
-	    System.out.println(privateVar);
+	    scopeInstance.useInner();
+
+	    ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+//	    System.out.println("varThree is not accessible here " + innerClass.varThree); varThree is private - not visible
+	    System.out.println("scopeInstance varOne is " + scopeInstance.getVarOne());
+	    System.out.println(varFour);
 
 	    scopeInstance.timesTwo();
-	    System.out.println("***********");
-	    ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
-	    innerClass.timesTwo();
+//	    System.out.println("***********");
+//	    ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+//	    innerClass.timesTwo();
     }
 }
--- Scope/src/com/example/test/ScopeCheck.java
+++ Scope/src/com/example/test/ScopeCheck.java
@@ -1,36 +1,41 @@
 package com.example.test;
 public class ScopeCheck {
     public int publicVar = 0;
-    private int privateVar = 1;
+    private int varOne = 1;
 
     public ScopeCheck() {
-        System.out.println("ScopeCheck created, publicVar = " + publicVar + ": privateVar = " + privateVar);
+        System.out.println("ScopeCheck created, publicVar = " + publicVar + ": varOne = " + varOne);
     }
 
-    public int getPrivateVar() {
-        return privateVar;
+    public int getVarOne() {
+        return varOne;
     }
 
     public void timesTwo() {
-        int privateVar = 2;
+        int varTwo = 2;
         for(int i = 0; i < 10; i++) {
-            System.out.println(i + " times two is " + i*privateVar); //this.privateVar will refer to the variable at the greater scope - the field
+            System.out.println(i + " times two is " + i*varTwo); //this.privateVar will refer to the variable at the greater scope - the field
         }
         //System.out.println("Value of i is now " + i); Scope of i is restricted to the for loop
 
     }
 
+    public void useInner() {
+        InnerClass innerClass = new InnerClass();
+        System.out.println("varThree from outer class: " + innerClass.varThree);
+    }
+
     public class InnerClass {
-        public int privateVar = 3;
+        private int varThree = 3;
 
         public InnerClass() {
-            System.out.println("InnerClass created, privateVar is " + privateVar);
+            System.out.println("InnerClass created, varOne is " + varOne + " and varThree is " + varThree);
         }
 
         public void timesTwo() {
-            int privateVar = 2; //delete this and it will go to the 3 in the InnerClass field
+//            int privateVar = 2; //delete this and it will go to the 3 in the InnerClass field
             for (int i = 0; i<10; i++) {
-                System.out.println(i + " times two is " + i*privateVar); //this.privateVar will look at 3 in InnerClass field
+                System.out.println(i + " times two is " + i*varThree); //this.privateVar will look at 3 in InnerClass field
                 //ScopeCheck.this.privateVar will look at the initial field instead
             }
         }
