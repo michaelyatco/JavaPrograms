9cfce079d96c400bb017d215e8ad86ce7d8667cf
michaelyatco <mjyatco@gmail.com>
2018-02-17 12:31:45 -0500

    Access modifiers set to public

---
+++ AccessModifiers/src/com/example/test/Main.java
@@ -0,0 +1,16 @@
+package com.example.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Account timsAccount = new Account("Tim");
+	    timsAccount.deposit(1000);
+	    timsAccount.withdraw(500);
+	    timsAccount.withdraw(-200);
+	    timsAccount.deposit(-20);
+	    timsAccount.calculateBalance();
+	    timsAccount.balance = 5000; //Because it's stated as public
+
+	    System.out.println("Balance on account is " + timsAccount.getBalance());
+    }
+}
