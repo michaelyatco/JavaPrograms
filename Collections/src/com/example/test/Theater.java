8465f5edca3e374bbf7753312e1d76c08ad77be2
michaelyatco <mjyatco@gmail.com>
2018-02-19 10:43:19 -0800

    Collections

---
+++ Collections/src/com/example/test/Theater.java
@@ -0,0 +1,57 @@
+package com.example.test;
+
+import java.util.*;
+
+public class Theater {
+    private final String theaterName;
+    private List<Seat> seats = new ArrayList<>();
+
+    public Theater(String theaterName, int numRows, int seatsPerRows) {
+        this.theaterName = theaterName;
+
+        int lastRow = 'A' + (numRows - 1);
+        for (char row = 'A'; row <= lastRow; row++) {
+            for (int seatNum = 1; seatNum <= seatsPerRows; seatNum++) {
+                Seat seat = new Seat(row + String.format("%02d", seatNum));
+                seats.add(seat);
+            }
+        }
+    }
+
+    public String getTheaterName() {
+        return theaterName;
+    }
+
+    public boolean reserveSeat(String seatNumber) {
+        Seat requestedSeat = null;
+        for(Seat seat : seats) {
+            if(Seat.getSeatNumber().equals(seatNumber)) {
+                requestedSeat = seat;
+                break;
+            }
+        }
+
+        if(requestedSeat == null) {
+            System.out.println("There is no seat " + seatNumber);
+            return false;
+        }
+
+        return requestedSeat.reserve();
+    }
+
+    //for testing
+    public void getSeats() {
+        for(Seat seat : seats) {
+            System.out.println(seat.getSeatNumber());
+        }
+    }
+
+    private class Seat {
+        private final String seatNumber;
+        private boolean reserved = false;
+
+        public Seat(String seatNumber) {
+            this.seatNumber = seatNumber;
+        }
+    }
+}
