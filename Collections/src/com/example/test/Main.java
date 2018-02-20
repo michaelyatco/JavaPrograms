df37e17b6eea4ed320da042ea6bfb64f604235b1
michaelyatco <mjyatco@gmail.com>
2018-02-20 09:16:49 -0800

    Collections expanded

--- Collections/src/com/example/test/Main.java
+++ Collections/src/com/example/test/Main.java
@@ -3,6 +3,12 @@ package com.example.test;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Theater theater = new Theater("Olympian", 8, 12);
+        theater.getSeats();
+        if(theater.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
     }
 }
--- Collections/src/com/example/test/Theater.java
+++ Collections/src/com/example/test/Theater.java
@@ -53,5 +53,29 @@ public class Theater {
         public Seat(String seatNumber) {
             this.seatNumber = seatNumber;
         }
+
+        public boolean reserve() {
+            if(!this.reserved) {
+                this.reserved = true;
+                System.out.println("Seat " + seatNumber + " reserved");
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public boolean cancel() {
+            if(this.reserved) {
+                this.reserved = false;
+                System.out.println("Reservation of seat " + seatNumber + " cancelled");
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public String getSeatNumber() {
+            return seatNumber;
+        }
     }
 }
