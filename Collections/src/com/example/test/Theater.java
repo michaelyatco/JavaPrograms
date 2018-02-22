473325cbdc9618ab11c00806baa8b292136cc5f3
michaelyatco <mjyatco@gmail.com>
2018-02-22 14:13:06 -0500

    Brute force method replaced with binary search method within Collections framework

--- Collections/src/com/example/test/Theater.java
+++ Collections/src/com/example/test/Theater.java
@@ -23,20 +23,29 @@ public class Theater {
     }
 
     public boolean reserveSeat(String seatNumber) {
-        Seat requestedSeat = null;
-        for(Seat seat : seats) {
-            if(seat.getSeatNumber().equals(seatNumber)) {
-                requestedSeat = seat;
-                break;
-            }
-        }
-
-        if(requestedSeat == null) {
+        Seat requestedSeat = new Seat(seatNumber);
+        int foundSeat = Collections.binarySearch(seats, requestedSeat, null); //checks midpoint element; most efficient for sorted list
+        //no more than 10 checks over no more than 20 checks for over 1 million elements
+        if(foundSeat >= 0) {
+            return seats.get(foundSeat).reserve();
+        } else {
             System.out.println("There is no seat " + seatNumber);
             return false;
         }
-
-        return requestedSeat.reserve();
+//        for(Seat seat : seats) {
+//            System.out.println(".");
+//            if(seat.getSeatNumber().equals(seatNumber)) {
+//                requestedSeat = seat;
+//                break;
+//            }
+//        }
+//
+//        if(requestedSeat == null) {
+//            System.out.println("There is no seat " + seatNumber);
+//            return false;
+//        }
+//
+//        return requestedSeat.reserve();
     }
 
     // for testing
@@ -46,12 +55,17 @@ public class Theater {
         }
     }
 
-    private class Seat {
+    private class Seat implements Comparable<Seat>{//to avoid brute force
         private final String seatNumber;
         private boolean reserved = false;
 
         public Seat(String seatNumber) {
             this.seatNumber = seatNumber;
+        }
+
+        @Override
+        public int compareTo(Seat seat) {
+            return this.seatNumber.compareToIgnoreCase(seat.getSeatNumber());
         }
 
         public boolean reserve() {
